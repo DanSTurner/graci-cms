ff8fd8031c8bd3f4889d7f84c8f51c31cb7fe327
Dan S Turner <turner@nowwithmoredan.com>
2014-01-08 15:26:37 -0800

    New rails app graci-cms

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GraciCms::Application.config.secret_key_base = '995ddf1667f6426431a92111b36f8e5a8623d3987273fa7497695a6ef69c2e0b5b7b95ca376a2fc8908ba804023a621c4fc7dc73d82e64ea3cc3e81ddda19349'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GraciCms::Application.config.session_store :cookie_store, key: '_graci-cms_session'
